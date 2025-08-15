7300b48712cfdca3a6a646a7871c5a616c14d169
eurofox33 <recherche627251@gmail.com>
2025-08-15 21:37:43 +0200

    Update aptpakete.sh

--- aptpakete.sh
+++ aptpakete.sh
@@ -1,11 +1,7 @@
 # APT Pakete
 
-# --- Schritt 1: Paketlisten aktualisieren ---
-echo "Aktualisiere Paketlisten."
 sudo apt update
 
-# --- Schritt 2: Die gewünschten Anwendungen installieren ---
-echo "Installiere htop, git, curl, tmux, vim, und neovim"
 sudo apt install -y htop git curl tmux vim neovim
 
 # --- Schritt 3: Erfolgsmeldung ---
