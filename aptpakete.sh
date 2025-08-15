9c771c088513079c9284ecdcef3639444f58ea01
eurofox33 <recherche627251@gmail.com>
2025-08-15 21:39:23 +0200

    Update aptpakete.sh

--- aptpakete.sh
+++ aptpakete.sh
@@ -1,7 +1,9 @@
 # APT Pakete
 
+# --- Schritt 1: Paketlisten aktualisieren ---
 sudo apt update
 
+# --- Schritt 2: Die gewünschten Anwendungen installieren ---
 sudo apt install -y htop git curl tmux vim neovim
 
 # --- Schritt 3: Erfolgsmeldung ---
